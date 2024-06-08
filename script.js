038e1dc18b8f937186bd972f8b7032e00e9d593a
KrzysztofLight <krzysztof.luksza@gmail.com>
2024-06-08 21:14:54 +0200

    Added '%', fixed floating point

--- script.js
+++ script.js
@@ -111,18 +111,44 @@ button_divide.addEventListener('click', function() {
     }
 })
 
+let button_percent = document.getElementById('percent');
+button_percent.addEventListener('click', function() {
+    if (checkLastSign()) {
+        display += '%';
+        display = display.slice(0, -1);
+        document.getElementById('displayInput').value = display;
+    }
+    else{
+        display += '%';
+        document.getElementById('displayInput').value = display;
+    }
+})
+
 function checkLastSign() {
     let lastSign = display.slice(-1);
-    if (lastSign == '+' || lastSign == '-' || lastSign == '*' || lastSign == '/') {
+    if (lastSign == '+' || lastSign == '-' || lastSign == '*' || lastSign == '/' || lastSign == '%') {
         return true;
     }
     return false;
 }
 
 
+
 let lastEqualtion = '';
 
+function preciseAddition(num1, num2) {
+    let factor = Math.pow(10, Math.max(decimalPlaces(num1), decimalPlaces(num2)));
+    return (num1 * factor + num2 * factor) / factor;
+}
 
+function decimalPlaces(num) {
+    let match = ('' + num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+    if (!match) { return 0; }
+    return Math.max(
+         0,
+         (match[1] ? match[1].length : 0)
+         - (match[2] ? +match[2] : 0));
+}
 
 button_equals.addEventListener('click', function() {
     if (display.includes('/0')) {
@@ -131,10 +157,17 @@ button_equals.addEventListener('click', function() {
     }
     lastEqualtion = display;
     document.getElementById('displayPrevEval').value = lastEqualtion + '=';
-    let result = eval(display);
-    display = result.toString();
-    document.getElementById('displayInput').value = display;
-    console.log(display);
+    
+    let equationParts = display.split('+');
+    if (equationParts.length > 1) {
+        let result = preciseAddition(parseFloat(equationParts[0]), parseFloat(equationParts[1]));
+        display = result.toString();
+        document.getElementById('displayInput').value = result;
+    } else {
+        let result = eval(display.replace(/%/g, '*0.01*'));
+        display = result.toString();
+        document.getElementById('displayInput').value = result;
+    }
 })
 
 button_clear.addEventListener('click', function() {
